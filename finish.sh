9ba9387d29b361f218fac32efe037c01e11d1ae4
joaco <joaquinramirez@gmail.com>
2024-06-24 20:11:28 -0700

    Add Finish.sh

---
+++ finish.sh
@@ -0,0 +1,27 @@
+
+# Install tomnomnom tools
+go install -v github.com/tomnomnom/anew@latest
+go install -v github.com/tomnomnom/unfurl@latest
+go install -v github.com/tomnomnom/gf@latest
+go install -v github.com/tomnomnom/gron@latest
+go install -v github.com/tomnomnom/meg@latest
+
+# Install ProjectDiscovery
+go install -v  github.com/projectdiscovery/httpx/cmd/httpx@latest
+go install -v  github.com/projectdiscovery/naabu/cmd/naabu@latest
+go install -v  github.com/projectdiscovery/katana/cmd/katana@latest
+go install -v  github.com/projectdiscovery/shuffledns/cmd/shuffledns@latest
+go install -v  github.com/d2mondev/puredns/v2@latest
+go install -v  github.com/ffuf/ffuf@latest
+
+# Install zero-tier 
+
+curl -s 'https://raw.githubusercontent.com/zerotier/ZeroTierOne/master/doc/contact%40zerotier.com.gpg' | gpg --import && \
+if z=$(curl -s 'https://install.zerotier.com/' | gpg); then echo "$z" | sudo bash; fi
+
+
+# Install nfs mount
+
+apt install nfs-common
+
+
